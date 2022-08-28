300810b860cce87bc860f082398edd839bded563
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-08-28 22:07:05 +0100

    modified code

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ $(document).ready(function(){
             url:`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}`,
         }).done(function(weatherdata){
          //console.log(weatherdata);
+         const weatherTemp =(weatherdata.main.temp -273).toFixed(2)
+         const feelsLike = (weatherdata.main.feels_like -273).toFixed(2)
          $("#profile").html(`
          <div class="row">
          <div class="card" style="width: 18rem;">
                 <img src=" http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${weatherdata.weather[0].description}</h5>
-                    <p class="card-text">The temperature at ${cityname} is = ${weatherdata.main.temp}&#8451 and it feels like${weatherdata.main.feels_like} &#8451</p>
+                    <p class="card-text">The temperature at ${cityname} is = ${weatherTemp}&#8451 and it feels like ${feelsLike} &#8451</p>
                     <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">Learn more about this place</a>
                 </div>
                 </div>
